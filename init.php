8ac49e56776892dc34e215fcb8af5ab6df52f7aa
mr-torgue <42840966+mr-torgue@users.noreply.github.com>
2023-09-13 09:30:50 +1000

    Update init.php

--- init.php
+++ init.php
@@ -18,16 +18,52 @@ class Mail_Filter extends Plugin {
 	}
 
 	function hook_article_filter_action($article, $action) {
-		// if ($action == "...") { .... }
-
-		$article["title"] = "[EXAMPLE FILTER WAS HERE: $action] " . $article["title"];
-
+		if($action == "send_mail_notification") { 
+			$this->send_notification($article, 'Alert');
+		}
 		return $article;
 	}
 
 	function hook_action_item() {
 		return "testing";
 	}
+
+	/*
+ 	sends a notification to the email address of the user that is logged in.
+  	TODO: allow user to specify custom email address
+   	TODO: allow user to specify from email address
+    	TODO: specify the filter it was triggered on
+	*/
+	function send_notification(array $article, string $notification_type) : void {	
+		$subject = $notification_type . ": " . $article['title'];
+		$content = "Alert triggered on the following article: " . $article;
+		$sth = $this->pdo->prepare("SELECT email, full_name FROM ttrss_users WHERE id = ?");
+		$sth->execute([$_SESSION['uid']]);
+		if ($row = $sth->fetch()) {
+			$reply = array();
+			$user_email = htmlspecialchars($row['email']);
+			$user_name = htmlspecialchars($row['full_name']);
+
+			$to = $user_email;
+			$subject = strip_tags($subject);
+			$message = strip_tags($content);
+			$from = "notify-ttrss@local.host";
+			
+			$mailer = new Mailer();
+			$rc = $mailer->mail(["to_address" => $to,
+				"headers" => ["Reply-To: $from"],
+				"subject" => $subject,
+				"message" => $message]);
+	
+			if (!$rc) {
+				$reply['error'] =  $mailer->error();
+			} else {
+				$reply['message'] = "UPDATE_COUNTERS";
+			}
+			print json_encode($reply);
+		}
+	}
+
 
 	function api_version() {
 		return 2;
